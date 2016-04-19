bb4894e1230228c6a2ebadf7109b9c0afd4d57d4
infamousbazooka <magnusfernandes1295@gmail.com>
2016-04-19 21:26:09 +0530

    Booking done

--- book.php
+++ book.php
@@ -1,1 +1,30 @@
-book.php
+<?php
+	require "connect.php";
+	require 'phpmailer/PHPMailerAutoload.php';
+	$date = date('Y-m-d H:i:s');
+	$startdate = $_POST['date'];
+	$email = $_POST['email'];
+	$contact = $_POST['contact'];
+	$days = $_POST['days'];
+	$sql = "INSERT into bookings (email, `date`, days, start_date, contact)VALUES ('" . $email . "', '" . $date . "', '" . $days . "', '" . $startdate . "', '" . $contact . "')";
+	if ($conn->query($sql) === TRUE) {
+		$body = "Hello Brits Palace!! My email address is " . $email . " and my contact number is " . $contact . ". I would like to book your home from " . $startdate . " for " . $days . ".";
+		$subject="Booking";
+		$mail = new PHPMailer();
+		$mail->IsHTML(true); 
+		$mail->Host = "relay-hosting.secureserver.net";
+		$mail->From = $email;
+		$mail->Subject = $subject;
+		$mail->Body = $body;
+		$mail->WordWrap = 50;
+		$mail->AddAddress("magnusfernandes1295@gmail.com");
+		if(!$mail->send()) {
+			echo "There was an error processing your request. Please contact us on +91-9158-857777.";
+		    echo 'Mailer Error: ' . $mail->ErrorInfo;
+		} else{
+			echo "<br>Booking placed. We will get back to you shortly.";
+		}
+	} else {
+		echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+?>
