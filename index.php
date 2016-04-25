140d193e95fbfb2fd4c752b78d3db862f62c7655
infamousbazooka <magnusfernandes1295@gmail.com>
2016-04-25 23:25:41 +0530

    Improved the loader

--- index.php
+++ index.php
@@ -85,6 +85,7 @@
                 width: 100%;
                 height: 100%;
                 opacity: 0.5;
+                cursor: pointer;
             }
             @-webkit-keyframes spin{
                 0%{
